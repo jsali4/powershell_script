ebd1d117a4ff1f3e70cdea2f0f68d41bded3047a
Ali <98909100+jsali4@users.noreply.github.com>
2022-07-12 17:41:28 +0300

    Update file.ps1

--- file.ps1
+++ file.ps1
@@ -1,11 +1,16 @@
 #код говно
 cls
+
+[Console]::outputEncoding = [System.Text.Encoding]::GetEncoding('cp866')
+[Console]::outputEncoding
 Write-Host 'Подожите'
+
+#Переменные путей директорий и файлов
 $path_main_folder = "C:\test_folder"
 $path_uninstall_file = "$path_main_folder\uninstall.txt"
 
+# Создание и наполнение файлов и директорий нужными данными
 $null = New-Item $path_main_folder -ItemType Directory
-
 $list_installed_apps = [System.Collections.ArrayList]@()
 foreach ($item in (wmic product get name)){
     if ($item -ne ''){
@@ -14,11 +19,14 @@ foreach ($item in (wmic product get name)){
 }
 Set-Content $path_uninstall_file $list_installed_apps
 
+#Проверка окончания изменения файла
 cls
-Write-Host 'Введите любую строку когда укажете в файле все приложения которые нужно удалить'
+$list_installed_apps
+Write-Host 'Введите любую строку когда оставите в файле все приложения которые нужно удалить'
 $null = Read-Host 'Сюда'
 cls
 
+#Финал(удаление)
 $list_uninstalling_apps = Get-Content $path_uninstall_file
 Write-Host $list_uninstalling_apps
 if ((Read-Host 'Все ли ок?(Y - yes, N-No)') -eq 'Y'){
